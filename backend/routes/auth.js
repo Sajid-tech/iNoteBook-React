c8cdf3907625ecb9ac9659dd036f90ec1e7c6e06
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-06-24 18:04:36 +0530

    password hashing,salt & jwt token add succesfuly

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,14 @@ const router = express.Router()
 const User = require('../models/User')
 // For Validation
 const { body, validationResult } = require('express-validator');
+// For password hashing
+const bcrypt = require('bcryptjs')
+//JWT token
+const jwt = require('jsonwebtoken')
+//JWT secret
+const JWT_SECRET = "sajidisgoodboy"
+
+
 
 // Create a User using: POST endpoint:"/api/auth/createuser". No login require
 router.post('/createuser', [
@@ -15,23 +23,38 @@ router.post('/createuser', [
     if (!error.isEmpty()) {
         return res.status(400).json({ error: error.array() })
     }
-    // check wheather the user with this email exits already
-    try {
 
+    try {
+        // check wheather the user with this email exits already
         let user = await User.findOne({ email: req.body.email })
 
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exits" })
         }
+        //password hashing
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt)
 
         //create a new user
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email
         })
-        // send response
-        res.json(user)
+        // send token and that taken have user id 
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        //jwt auth sign
+        const authToken = jwt.sign(data, JWT_SECRET)
+
+        // send response --auth token not user
+
+        // res.json(user)
+        res.json({ authToken })
         // catch the error
     } catch (error) {
         console.error(error.message)
