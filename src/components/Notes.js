709eb97defd3c6a24980a5c68cba5f5032dfc96d
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-06-29 03:54:08 +0530

    editNote function use and call with api succesfully

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,21 +1,91 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import noteContext from '../context/notes/noteContext'
 import NoteItem from './NoteItem';
 import AddNote from './AddNote';
 
 const Notes = () => {
     const context = useContext(noteContext);
-    const { notes, getNotes } = context;
+    const { notes, getNotes, editNote } = context;
     useEffect(() => {
         getNotes()
+        // eslint-disable-next-line
     }, [])
+    const [note, setNote] = useState({ id: '', etitle: "", edescription: "", etag: "" })
+    const ref = useRef(null)
+    const refClose = useRef(null)
+    const updateNote = (currentNote) => {
+        ref.current.click()
+        //ye modal jb khulega toh data rhega usme
+        setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag })
+    }
+    // modal form function start
+
+
+    const handleClick = () => {
+        console.log("updating the note...", note)
+        editNote(note.id, note.etitle, note.edescription, note.etag)
+        //ye fucntion jaise hi update pe click hoga modal close ho jayega
+        refClose.current.click()
+
+    }
+    const onChange = (e) => {
+        setNote({ ...note, [e.target.name]: e.target.value })
+    }
+
+    //modal form function end
+
     return (
         <>
             <AddNote />
+            {/* Modal Start */}
+
+            <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            {/* Modal form start */}
+
+                            <form className='my-3'>
+                                <div className="mb-3">
+                                    <label htmlFor="title" className="form-label">Title</label>
+                                    <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange} />
+
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="description" className="form-label">Description</label>
+                                    <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="tag" className="form-label">Tag</label>
+                                    <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} />
+                                </div>
+
+
+                            </form>
+
+                            {/* Modal form end */}
+                        </div>
+                        <div className="modal-footer">
+                            <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" onClick={handleClick} className="btn btn-primary">Update note</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Modal End */}
             <div className="row my-3">
                 <h2>Your Notes</h2>
                 {notes.map((note) => {
-                    return <NoteItem key={note._id} note={note} />
+                    return <NoteItem key={note._id} updateNote={updateNote} note={note} />
                 })}
             </div>
         </>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -17,7 +17,7 @@ const NoteState = (props) => {
             }
         });
         const json = await response.json()
-        console.log(json)
+        console.log("getdata=", json)
         setNotes(json)
     }
 
@@ -32,7 +32,10 @@ const NoteState = (props) => {
                 "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ5NmUwMDAxYjZjYTJjYWE2ZDA5YzBhIn0sImlhdCI6MTY4NzYxNTE1MX0.xfvlZuBZu1nLK8u9Pa3mWZTi342T5cr8y5CVCjG9whA"
             },
             body: JSON.stringify({ title, description, tag })
+
         });
+        const json = await response.json();
+        console.log("addNote =", json)
 
 
         console.log("Adding a new note")
@@ -49,8 +52,21 @@ const NoteState = (props) => {
     }
 
     // Delete a Note
-    const deleteNote = (id) => {
+    const deleteNote = async (id) => {
         // TODO: API Call
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ5NmUwMDAxYjZjYTJjYWE2ZDA5YzBhIn0sImlhdCI6MTY4NzYxNTE1MX0.xfvlZuBZu1nLK8u9Pa3mWZTi342T5cr8y5CVCjG9whA"
+            },
+            // body: JSON.stringify({ title, description, tag })
+        });
+        const json = response.json();
+        console.log("editing note=", json)
+
+
+        //logic to delete the note
         console.log("Deleting the note with id" + id);
         const newNotes = notes.filter((note) => { return note._id !== id })
         setNotes(newNotes)
@@ -59,25 +75,31 @@ const NoteState = (props) => {
     const editNote = async (id, title, description, tag) => {
         // API Call 
         const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-            method: 'POST',
+            method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
                 "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ5NmUwMDAxYjZjYTJjYWE2ZDA5YzBhIn0sImlhdCI6MTY4NzYxNTE1MX0.xfvlZuBZu1nLK8u9Pa3mWZTi342T5cr8y5CVCjG9whA"
             },
             body: JSON.stringify({ title, description, tag })
         });
-        const json = response.json();
+        const json = await response.json();
+        console.log("editing note=", json)
 
+        let newNotes = JSON.parse(JSON.stringify(notes))
         // Logic to edit in client
         for (let index = 0; index < notes.length; index++) {
             const element = notes[index];
             if (element._id === id) {
-                element.title = title;
-                element.description = description;
-                element.tag = tag;
+                newNotes[index].title = title;
+                newNotes[index].description = description;
+                newNotes[index].tag = tag;
+                break;
             }
 
+
+
         }
+        setNotes(newNotes)
     }
 
     return (
