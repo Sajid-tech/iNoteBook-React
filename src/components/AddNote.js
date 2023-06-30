edf8d0213f618d16ecb52c178aeda45c88240f69
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-06-30 20:12:59 +0530

    update ui

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -23,16 +23,16 @@ const AddNote = (props) => {
                 <form className='my-3'>
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" value={note.title} onChange={onChange} minLength={5} required />
+                        <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" placeholder="min 5 char" value={note.title} onChange={onChange} minLength={5} required />
 
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Description</label>
-                        <input type="text" className="form-control" id="description" name="description" value={note.description} onChange={onChange} minLength={5} required />
+                        <input type="text" className="form-control" id="description" name="description" value={note.description} placeholder="min 5 char" onChange={onChange} minLength={5} required />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="tag" className="form-label">Tag</label>
-                        <input type="text" className="form-control" id="tag" name="tag" value={note.tag} onChange={onChange} minLength={5} required />
+                        <input type="text" className="form-control" id="tag" name="tag" value={note.tag} onChange={onChange} placeholder="min 5 char" minLength={5} required />
                     </div>
 
                     <button disabled={note.title.length < 5 || note.description.length < 5} type="submit" className="btn btn-primary" onClick={handleClick} >Add Note</button>
